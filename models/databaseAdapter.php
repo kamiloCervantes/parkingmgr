881f80e3f89bd920045d09982b3c49d91e9a87b7
Kamilo Cervantes <cacesa8931@gmail.com>
2011-11-11 22:38:48 -0500

    Change-Id: I832e2af51d3151dcefb3475e88b6c2b9aa0ee38d

--- models/databaseAdapter.php
+++ models/databaseAdapter.php
@@ -6,11 +6,7 @@
 $rootPath = dirname(dirname(__FILE__));
 set_include_path(get_include_path() . PATH_SEPARATOR . $rootPath . '/controllers' . PATH_SEPARATOR
 									. PATH_SEPARATOR . '/models' . PATH_SEPARATOR . '/views' . PATH_SEPARATOR);
-/**
- * Description of DbConnecting
- *Por favor, permíteme ver cuándo te conectas
- * @author luchox25
- */
+
 include_once 'conf/dbconf.php';									
 class databaseAdapter {
 
@@ -41,18 +37,6 @@ class databaseAdapter {
 		return self::$INSTANCIA_DE_CLASE;
 	}
 
-	public function getDatabaseManager(){
-		return $this->_databaseManager;
-	}
-	
-	//Conectarse con una base de datos en Mysql
-	private function connectMsql(){
-		$this->_conn = mysql_connect($this->_server, $this->_user, $this->_password)
-		or die(mysql_error());
-		mysql_select_db($this->_database, $this->_conn);
-                return $this->_conn;
-	}
-
 	//Conectarse con una base de datos en Postgresql
 	private function connectPostgres(){
 		$this-> _conn=pg_connect("host=".$this->_server.
@@ -61,48 +45,10 @@ class databaseAdapter {
 					 " password=".$this->_password."")
 				         or die("<br>Error: Conexion fallida.<br>");
 	}
-	
-	//Metodo para establecer una conexion con el gestor de base de datos(Soporta postgres o mysql)
-        public function connect(){
-        	$okconnect = false;
-            switch ($this->_databaseManager){
-                case 'mysql':
-                    $this->connectMsql();
-                    $okconnect = true;
-                    break;
-                case 'postgresql':
-                    $this->connectPostgres();
-                    $okconnect = true;
-                    break;
-            }
-			return $okconnect;
-        }
         
         private function  closePostgres(){
         	pg_close($this->_conn);
         }
-        
-		private function  closeMysql(){
-        	mysql_close($this->_conn);
-        }
-        
-        public function close(){
-         switch ($this->_databaseManager){
-                case 'mysql':
-                    return $this->closeMysql();
-                    break;
-                case 'postgresql':
-                    return $this->closePostgres();
-                    break;
-            }
-        }
-        
-	//Ejecutar consulta en mysql
-    private function executeMsql($query)
-	{
-		$respuesta = mysql_query($query, $this->_conn) or die(mysql_error());
-		return $respuesta;
-	}
 
 	//Ejecutar consulta en postgresql
 	private function executePostgres($query){
@@ -111,141 +57,9 @@ class databaseAdapter {
                 return $respuesta;
 	}
 
-	//Ejecutar una consulta(Postgres o Mysql)
-    public function execute($query, $invocador="ninguno"){
-    	//echo "<h1>$query</h1>";
-            switch ($this->_databaseManager){
-                case 'mysql':
-                    return $this->executeMsql($query);
-                    break;
-                case 'postgresql':
-                    return $this->executePostgres($query);
-                    break;
-            }
-        }
-
     private function countRowPostgres($result){
     	return pg_num_rows($result);
     }
-    
-    private function countRowMysql($result){
-    	return mysql_num_rows($result);
-    }
-    
-    public function countRows($result){
-     		switch ($this->_databaseManager){
-                case 'mysql':
-                    return $this->countRowMysql($result);
-                    break;
-                case 'postgresql':
-                    return $this->countRowPostgres($result);
-                    break;
-            }
-    }
-    
-    private function retornarFilaPostgres($result){
-    	$fil = pg_fetch_assoc($result);
-    	return $fil;
-    }
-
-	private function retornarFilaMysql($result){
-    	$fil = mysql_fetch_assoc($result);
-    	return $fil;
-    }
-    
-    public  function returnId(){
-    	return mysql_insert_id($this->_conn);
-    }
-    
-    public function retornarFila($result){
-    		switch ($this->_databaseManager){
-                case 'mysql':
-                    return $this->retornarFilaMysql($result);
-                    break;
-                case 'postgresql':
-                    return $this->retornarFilaPostgres($result);
-                    break;
-            }
-    }
-    
-    
-    	public function fetchObject($result){
-    		switch ($this->_databaseManager){
-    			case 'mysql':
-                    return $this->fetchObjectMysql($result);
-                    break;
-                case 'postgresql':
-                    return $this->fetchObjectPostgres($result);
-                    break;
-    		}
-    	}
-    
-    	private function fetchObjectPostgres($result){
-    		$result = pg_fetch_object($result);
-    		return $result;
-    	}
-    	
-		private function fetchObjectMysql($result){
-    		$result = mysql_fetch_object($result);
-    		return $result;
-    	}
-    	
-    	public function free_Result($result){
-    		switch ($this->_databaseManager){
-    			case 'mysql':
-                    return $this->free_ResultMysql($result);
-                    break;
-                case 'postgresql':
-                    return $this->free_ResultPostgres($result);
-                    break;
-    		}
-    	}
-    	
-		private function free_ResultMysql($result){
-    		$res = mysql_free_result($result);
-    		return $res;
-    	}
-    	
-		private function free_ResultPostgres($result){
-    		$res = pg_free_result($result);
-    		return $res;
-    	}
-    	
-        public function setConnectDefault(){
-        	$info = new dbconf();
-      		$this->_databaseManager = $info->getDatabaseManager();
-      		$this->_database = $info->getDatabase();
-      		$this->_user = $info->getUser();
-      		$this->_password = $info->getPass();
-      		$this->_server = $info->getHost();
-        }
-        
-        public function setConnectDefaultXML(){
-		$xml = simplexml_load_file("../../models/conf/db.xml");
-                $cont = 0;
-                foreach ($xml->database as $db){
-                    $conf = utf8_decode($db['conf']);
-                    if(strcasecmp($conf, "default")==0){
-                        if($cont==0){
-                            $this->_databaseManager=$db['databasemanager'];
-                            $this->_server=$db->host;
-                            $this->_database=$db->dbname;
-                            $this->_user=$db->username;
-                            $this->_password=$db->clave;
-                        }
-                        $cont++;
-                    }
-                }
-                if($cont>1){
-                        echo "<b class=msgError>Error: Se han configurado mas de una base de datos como default</b><br/>";
-                        echo "<b class=msgAdvertencia>Abvertencia: Se tomara la primera configuracion hallada.</b><br/>";
-                    }
-                    else{
-                        if($cont<1){
-                            echo "<b class=msgError>Error de conexion: No se configuro ninguna base de datos como default</b><br/>";
-                        }
-                    }
-	}
 
 	public function __clone()
    	{
